8b9d65f9ddc6a0e20950cff12caa83d9a21f05c5
Dima Duc <dimaduc@gmail.com>
2019-10-30 09:19:48 +0700

    Updates

---
+++ precache-manifest.06a60f33c27bbc497e666e31b56a8d23.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "741a7fc7f646687f98349a214a1d4688",
+    "url": "/dima-star/index.html"
+  },
+  {
+    "revision": "2886364261c6e8f65977",
+    "url": "/dima-star/static/css/2.507930fa.chunk.css"
+  },
+  {
+    "revision": "abf72aa65d656ebe9b86",
+    "url": "/dima-star/static/css/main.2c2b0305.chunk.css"
+  },
+  {
+    "revision": "2886364261c6e8f65977",
+    "url": "/dima-star/static/js/2.e92387ec.chunk.js"
+  },
+  {
+    "revision": "abf72aa65d656ebe9b86",
+    "url": "/dima-star/static/js/main.7cecd161.chunk.js"
+  },
+  {
+    "revision": "5b654f7d3d12355f9702",
+    "url": "/dima-star/static/js/runtime-main.ad014889.js"
+  },
+  {
+    "revision": "8e0fbf2d794017b968252f9333e1e87d",
+    "url": "/dima-star/static/media/Aasajj_Ventress.8e0fbf2d.jpg"
+  },
+  {
+    "revision": "9270c286654112248095d51ee4801c16",
+    "url": "/dima-star/static/media/Ahsoka_Tano.9270c286.jpg"
+  },
+  {
+    "revision": "3d7c74ac555da8f19370d2cf1aaed0a7",
+    "url": "/dima-star/static/media/Anakin_Skywalker.3d7c74ac.jpg"
+  },
+  {
+    "revision": "5cc022543d3a0972a347062fb5fb6a65",
+    "url": "/dima-star/static/media/Anakin_Vader_Skywalker.5cc02254.jpg"
+  },
+  {
+    "revision": "7a98e7f85ee381aa2ed48af55570695a",
+    "url": "/dima-star/static/media/Darth_Maul.7a98e7f8.jpg"
+  },
+  {
+    "revision": "27e888a862cebcb8d3b3a2dc6cee452d",
+    "url": "/dima-star/static/media/Darth_Vader.27e888a8.jpg"
+  },
+  {
+    "revision": "3c6104063323e6c4fb2b2adab4c6884e",
+    "url": "/dima-star/static/media/Galen_Marek_(Starkiller).3c610406.jpg"
+  },
+  {
+    "revision": "c4a34dfe87d871a3b3c4cc438c8eaef2",
+    "url": "/dima-star/static/media/Gray_Jedi_(Jedi_&_Sith)_symbol.c4a34dfe.png"
+  },
+  {
+    "revision": "8985fd3a628b1ac7017b70d3d914d961",
+    "url": "/dima-star/static/media/Jolee_Bindo.8985fd3a.jpg"
+  },
+  {
+    "revision": "76198e723113b5db8236b141ebc7eeea",
+    "url": "/dima-star/static/media/Komari_Vosa.76198e72.jpg"
+  },
+  {
+    "revision": "004c2a3cce1fe7346c3834ddd5d6e5c6",
+    "url": "/dima-star/static/media/Leia_Skywalker.004c2a3c.jpg"
+  },
+  {
+    "revision": "8662fc3cd6609300f9a69be4b31634a4",
+    "url": "/dima-star/static/media/Lightsaber Double Bladed.8662fc3c.jpg"
+  },
+  {
+    "revision": "0085984e61cc2b136ab796ff0f4ba849",
+    "url": "/dima-star/static/media/Lightsaber Krayt Dragon.0085984e.jpg"
+  },
+  {
+    "revision": "d0f359fad6a0db7a3120c91fe8c48fdf",
+    "url": "/dima-star/static/media/Lightsabers (One Hand or Two Hand).d0f359fa.jpg"
+  },
+  {
+    "revision": "19c589e4973b55bc02eab647a3b4172c",
+    "url": "/dima-star/static/media/Luke_Skywalker.19c589e4.jpg"
+  },
+  {
+    "revision": "63edc8ac893900aa47140e0db1a0f37f",
+    "url": "/dima-star/static/media/Mara_Jade.63edc8ac.jpg"
+  },
+  {
+    "revision": "5f8298d5ba69f3ee04c2041b9e30de5c",
+    "url": "/dima-star/static/media/Qui_Gon_Jinn.5f8298d5.jpg"
+  },
+  {
+    "revision": "063bee19b5006f24beb8085bdcbb63b2",
+    "url": "/dima-star/static/media/Revan.063bee19.jpg"
+  },
+  {
+    "revision": "05ba2845275c1667820d67afa716596e",
+    "url": "/dima-star/static/media/Savage_Opress.05ba2845.jpg"
+  },
+  {
+    "revision": "a9446d3fcdc3e4b27512c2ac67c23721",
+    "url": "/dima-star/static/media/Sith_Padme.a9446d3f.jpg"
+  },
+  {
+    "revision": "e1be4ff91224495793e6d68e0d306d94",
+    "url": "/dima-star/static/media/Symbol Jedi.e1be4ff9.png"
+  },
+  {
+    "revision": "ad9bef92e3fa94f034e20480184c3d33",
+    "url": "/dima-star/static/media/Symbol Sith.ad9bef92.png"
+  },
+  {
+    "revision": "0430331b2e63ce822c82d4ea0bd91ca3",
+    "url": "/dima-star/static/media/Tahl.0430331b.jpg"
+  },
+  {
+    "revision": "c3ecfc15a3f7ffcb4ac6b8bc03f83e25",
+    "url": "/dima-star/static/media/Westar-34.c3ecfc15.jpg"
+  },
+  {
+    "revision": "a317be3adf829083f5e829ec5a21f355",
+    "url": "/dima-star/static/media/Westar-35.a317be3a.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/dima-star/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/dima-star/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/dima-star/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/dima-star/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/dima-star/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/dima-star/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/dima-star/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/dima-star/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/dima-star/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/dima-star/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/dima-star/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/dima-star/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/dima-star/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/dima-star/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/dima-star/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/dima-star/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/dima-star/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
